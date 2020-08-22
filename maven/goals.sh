a7c7c5527f74bdb5381fb4e65f7d2aa6795b5331
Kunal Rustagi <kunal@Kunals-MacBook-Air.local>
2020-08-22 17:25:02 +0530

    Added assignments to the same repo

---
+++ maven/goals.sh
@@ -0,0 +1,1 @@
+mvn help:describe -DgroupId=org.apache.maven.plugins -DartifactId=${1:-maven-war-plugin} -Ddetail=false
---
+++ maven/local.sh
@@ -0,0 +1,2 @@
+mvn install:install-file -Dfile=/Users/kunal/Desktop/maven/local.jar -DgroupId=poi -DartifactId=poi -Dpackaging=jar -Dversion=1.0
+
